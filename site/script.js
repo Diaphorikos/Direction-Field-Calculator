c438051ff1c775fb00596398241d3c0be8d734c8
Richard Yi <richardyi25@gmail.com>
2019-01-13 22:49:10 -0500

    curve kinda works

--- site/script.js
+++ site/script.js
@@ -1,4 +1,4 @@
-var inputField, c, maxx, maxy, points;
+var inputField, c, maxx, maxy, maxw, points, samples;
 
 function drawSlope(length, slope, xc, yc){
 	c.drawLine({
@@ -22,6 +22,8 @@ function draw(data){
 	}
 	console.log(slopes);
 
+	/*
+
 	var canvas = document.getElementById('canvas');
 	var graph = document.getElementById('graph');
 	canvas.width = graph.clientWidth;
@@ -36,11 +38,13 @@ function draw(data){
 		fromCenter: false
 	});
 
+	*/
+
 	var adj = (maxx - maxy) / 2;
 
 	for(var i = 0; i < points; i++){
 		for(var j = 0; j < points; j++){
-			if(slopes[i][j] == 'nan' || slopes[i][j] == '-nan' || slopes[i][j] == 'inf' || slopes[i][j] == '-inf')
+			if(/*slopes[i][j] == 'nan' || slopes[i][j] == '-nan' ||*/ slopes[i][j] == 'inf' || slopes[i][j] == '-inf')
 				slopes[i][j] = 9999;
 			drawSlope(maxy / (points + 3), slopes[i][j] * 1, adj + (j + 1)/(points + 1) * maxy, (i + 1)/(points + 1) * maxy);
 		}
@@ -54,8 +58,9 @@ function getSlopes(data){
 
 	var s = '0\n';
 	s += data + '\n';
-	s += '-2\n2\n-2\n2\n39\n39\n';
-	points = 39;
+	s += '-4\n4\n-4\n4\n40\n40\n';
+	maxw = 3;
+	points = 40;
 
 	$.get('request.php',
 		{
@@ -66,7 +71,7 @@ function getSlopes(data){
 	);
 }
 
-function submit(){
+function getPolish(){
 	var s = inputField.latex();
 	s = s.split('\\ ').join('');
 	console.log(s);
@@ -80,6 +85,62 @@ function submit(){
 	);
 }
 
+function drawCurve(data){
+	var canvas = document.getElementById('canvas');
+	var graph = document.getElementById('graph');
+	canvas.width = graph.clientWidth;
+	canvas.height = graph.clientHeight;
+	maxx = canvas.width;
+	maxy = canvas.height;
+
+	var raw = data.split('\n');
+	var curve = [];
+
+	for(var i = 0; i < samples; i++){
+		var temp = raw[i].split(' ');
+		curve.push(temp);
+	}
+
+	var adj = (maxx - maxy)/2;
+
+	for(var i = 0; i < samples - 1; i++){
+		var x1 = curve[i][0] * 1;
+		var y1 = curve[i][1] * 1;
+		var x2 = curve[i + 1][0] * 1;
+		var y2 = curve[i + 1][1] * 1;
+
+		c.drawLine({
+			strokeStyle: 'black',
+			strokeWidth: 2,
+			x1: (x1 + 1) / 2 * maxy + adj,
+			y1: maxy - ((y1 + 1) / 2 * maxy),
+			x2: (x2 + 1) / 2 * maxy + adj,
+			y2: maxy - ((y2 + 1) / 2 * maxy)
+		});
+
+//		console.log(
+//			(x1 / maxw + 1) / 2 * maxy,
+//			maxy - ((y1 / maxw + 1) / 2 * maxy),
+//			(x2 / maxw + 1) / 2 * maxy,
+//			maxy - ((y2 / maxw + 1) / 2 * maxy)
+//		);
+	}
+}
+
+function getCurve(){
+	samples = 1000;
+	var s = '1\n( 0 x - ) y / \n-4\n4\n-4\n4\n1\n-1\n1000\n200';
+	
+	$.get("request.php",
+		{
+			type: "curve",
+			data : s
+		},
+		drawCurve
+	);
+}
+
+
 $(document).ready(function(){
 	var MQ = MathQuill.getInterface(2);
 
@@ -89,12 +150,15 @@ $(document).ready(function(){
 	var input = document.getElementById('input');
 	inputField = MQ.MathField(input);
 
-	$('#submit').click(submit);
+	$('#submit').click(getPolish);
 	$(document).keydown(function(e){
-		if(e.which == 13) submit();
+		if(e.which == 13) getPolish();
 	});
 
 	c = $('canvas');
+	maxw = 3;
+
+	getCurve();
 
 	$(document).dblclick(function(){
 		var ele = $('div');
